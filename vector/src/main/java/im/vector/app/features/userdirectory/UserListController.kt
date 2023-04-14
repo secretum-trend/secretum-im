904de4e3fe43e6b1733a95230ac2d4d38bc13e05
Kevin <kevindev2020@gmail.com>
2023-04-14 15:04:15 +0700

    fix ui, and fixed gitignore

--- vector/src/main/java/im/vector/app/features/userdirectory/UserListController.kt
+++ vector/src/main/java/im/vector/app/features/userdirectory/UserListController.kt
@@ -62,38 +62,38 @@ class UserListController @Inject constructor(
         val host = this
 
         // Build generic items
-        if (currentState.searchTerm.isBlank()) {
-            if (currentState.showInviteActions()) {
-                actionItem {
-                    id(R.drawable.ic_share)
-                    title(host.stringProvider.getString(R.string.invite_friends))
-                    actionIconRes(R.drawable.ic_share)
-                    clickAction {
-                        host.callback?.onInviteFriendClick()
-                    }
-                }
-            }
-            if (currentState.showContactBookAction) {
-                actionItem {
-                    id(R.drawable.ic_baseline_perm_contact_calendar_24)
-                    title(host.stringProvider.getString(R.string.contacts_book_title))
-                    actionIconRes(R.drawable.ic_baseline_perm_contact_calendar_24)
-                    clickAction {
-                        host.callback?.onContactBookClick()
-                    }
-                }
-            }
-            if (currentState.showInviteActions()) {
-                actionItem {
-                    id(R.drawable.ic_qr_code_add)
-                    title(host.stringProvider.getString(R.string.qr_code))
-                    actionIconRes(R.drawable.ic_qr_code_add)
-                    clickAction {
-                        host.callback?.onUseQRCode()
-                    }
-                }
-            }
-        }
+//        if (currentState.searchTerm.isBlank()) {
+//            if (currentState.showInviteActions()) {
+//                actionItem {
+//                    id(R.drawable.ic_share)
+//                    title(host.stringProvider.getString(R.string.invite_friends))
+//                    actionIconRes(R.drawable.ic_share)
+//                    clickAction {
+//                        host.callback?.onInviteFriendClick()
+//                    }
+//                }
+//            }
+//            if (currentState.showContactBookAction) {
+//                actionItem {
+//                    id(R.drawable.ic_baseline_perm_contact_calendar_24)
+//                    title(host.stringProvider.getString(R.string.contacts_book_title))
+//                    actionIconRes(R.drawable.ic_baseline_perm_contact_calendar_24)
+//                    clickAction {
+//                        host.callback?.onContactBookClick()
+//                    }
+//                }
+//            }
+//            if (currentState.showInviteActions()) {
+//                actionItem {
+//                    id(R.drawable.ic_qr_code_add)
+//                    title(host.stringProvider.getString(R.string.qr_code))
+//                    actionIconRes(R.drawable.ic_qr_code_add)
+//                    clickAction {
+//                        host.callback?.onUseQRCode()
+//                    }
+//                }
+//            }
+//        }
 
         when (val matchingEmail = currentState.matchingEmail) {
             is Success -> {
